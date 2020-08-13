ce660a4558cd52c49f956e644992da846a129cb8
Danilo Peña <nicolasdpf@gmail.com>
2020-08-12 23:26:03 -0500

    First styles to Headbar

---
+++ src/components/Headbar/styles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  border-bottom: 1px solid var(--dark-color);
+  height: 89px;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
