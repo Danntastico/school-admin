a291024462671808bf1326d765079e2751da113c
Danilo Peña <nicolasdpf@gmail.com>
2020-08-12 20:46:53 -0500

    Setting the structure for sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import { Logo } from '../Logo';
+import { Link } from '@reach/router';
+import { MdHome, MdWork, MdLocalLibrary, MdLibraryBooks } from 'react-icons/md';
+import {
+  HOME_PATH,
+  TEACHER_PATH,
+  STUDENT_PATH,
+  SUBJECT_PATH,
+} from '../../utils/constants';
 
 export const Sidebar = () => {
   return (
@@ -6,8 +15,25 @@ export const Sidebar = () => {
       <div>
         <Logo />
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <Link to={HOME_PATH}>
+          <MdHome /> Home
+        </Link>
+        <Link to={TEACHER_PATH}>
+          <MdWork /> Teachers
+        </Link>
+        <Link to={STUDENT_PATH}>
+          <MdLocalLibrary />
+          Students
+        </Link>
+        <Link to={SUBJECT_PATH}>
+          <MdLibraryBooks />
+          Subjects
+        </Link>
+      </div>
+      <div>
+        <button>GENERATE REPORT</button>
+      </div>
     </nav>
   );
 };
