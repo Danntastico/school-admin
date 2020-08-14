8b2f4fc06236f4a9e77c57810dcf38216db9f8e9
Danilo Peña <nicolasdpf@gmail.com>
2020-08-14 11:22:41 -0500

    Created Paths to DetailView

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 //Router paths
 export const HOME_PATH = '/';
-export const STUDENT_PATH = 'students/*';
-export const TEACHER_PATH = 'teachers/*';
-export const SUBJECT_PATH = 'subjects/*';
+export const STUDENT_PATH = 'students/';
+export const TEACHER_PATH = 'teachers/';
+export const SUBJECT_PATH = 'subjects/';
+export const DETAIL_SUBJECT_PATH = 'subjects/detail/:id';
+export const DETAIL_STUDENT_PATH = 'teachers/detail/:id';
+export const DETAIL_TEACHER_PATH = 'students/detail/:id';
