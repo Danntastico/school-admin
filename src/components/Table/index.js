65cd9ab6a330d4d1a17114b061c83fee088ca271
Danilo Peña <nicolasdpf@gmail.com>
2020-08-14 11:22:19 -0500

    Modified cardContainer

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { TableContainer, TableHeader, TableContent } from './styles';
+import { TableHeader, TableContent } from './styles';
 import { Button } from '../Button';
+import { CardContainer } from '../CardContainer';
 
 export const Table = ({ title = 'Items', children }) => {
   return (
-    <TableContainer>
+    <CardContainer>
       <TableHeader>
         <h2>{title}</h2>
         <Button>Add New Item</Button>
       </TableHeader>
       <TableContent>{children}</TableContent>
-    </TableContainer>
+    </CardContainer>
   );
 };
--- src/components/Table/styles.js
+++ src/components/Table/styles.js
@@ -1,14 +1,5 @@
 import styled from 'styled-components';
 
-export const TableContainer = styled.div`
-  width: 100%;
-  height: auto;
-  background-color: #ffff;
-  border-radius: 20px;
-  box-shadow: 0px 5px 5px 0px rgba(207, 219, 213, 1);
-  padding-bottom: 70px;
-`;
-
 export const TableHeader = styled.div`
   align-items: center;
   display: flex;
