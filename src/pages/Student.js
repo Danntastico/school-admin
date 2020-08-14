ccb60c5802a07391864e137b7de0914d51cdea8b
Danilo Peña <nicolasdpf@gmail.com>
2020-08-14 11:22:33 -0500

    Added table into Student and Subjects

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from '@reach/router';
 import { Headbar } from '../components/Headbar';
+import { Table } from '../components/Table';
+import { TableItem } from '../components/TableItem';
+import { DETAIL_STUDENT_PATH } from '../utils/constants';
 
 export const Students = () => {
   return (
     <div>
       <Headbar title='Student View' />
-      <h1>Student</h1>
+      <div className='pageContent'>
+        <Table title='Active Students'>
+          <Link to={DETAIL_STUDENT_PATH}>
+            <TableItem />
+          </Link>
+        </Table>
+      </div>
     </div>
   );
 };
