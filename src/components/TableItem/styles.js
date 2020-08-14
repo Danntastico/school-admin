370b85e8db337ba70ba2aeb3504e3d24d8e5e553
Danilo Peña <nicolasdpf@gmail.com>
2020-08-13 22:55:55 -0500

    Testing tableItems

--- src/components/TableItem/styles.js
+++ src/components/TableItem/styles.js
@@ -1,12 +1,15 @@
-import React from 'react';
 import styled from 'styled-components';
 
 export const ItemContainer = styled.div`
-  position: relative;
-  height: 55px;
-  padding: 20px 0;
+  align-items: center;
   border-bottom: 1px solid var(--gray-color);
-
+  cursor: pointer;
+  border-radius: 5px;
+  display: flex;
+  height: 55px;
+  padding: 20px 20px;
+  position: relative;
+  transition: 0.4s;
   > *:not(:last-child) {
     display: inline;
     margin: 0 5px;
@@ -14,5 +17,26 @@ export const ItemContainer = styled.div`
   > :last-child {
     position: absolute;
     right: 0;
+  }
+  > svg,
+  p {
+    color: var(--dark-color);
+    font-size: 14px;
+    font-weight: 200;
+    text-transform: capitalize;
+  }
+  > svg {
+    font-size: 20px;
+  }
+
+  > p:before {
+    content: '·';
+    line-height: 20px;
+    font-size: 20px;
+    padding: 0 10px;
+  }
+
+  :hover {
+    background-color: var(--gray-color);
   }
 `;
