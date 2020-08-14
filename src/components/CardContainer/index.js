55070b12188fc015f50bc53906545d153adb658f
Danilo Peña <nicolasdpf@gmail.com>
2020-08-14 11:21:59 -0500

    Created a cardCOntainer for Reusability

---
+++ src/components/CardContainer/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Container } from './styles';
+export const CardContainer = ({ children }) => {
+  return <Container>{children}</Container>;
+};
---
+++ src/components/CardContainer/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: #ffff;
+  border-radius: 20px;
+  box-shadow: 0px 5px 5px 0px rgba(207, 219, 213, 1);
+  padding-bottom: 70px;
+`;
