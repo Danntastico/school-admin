67aea7ad8b180bfa7fe2307639690349c4b50f27
Danilo <nicolasdpf@gmail.com>
2020-08-16 10:58:00 -0500

    skiping disabled prop on initial fieldList

--- src/utils/fieldsList.js
+++ src/utils/fieldsList.js
@@ -2,31 +2,26 @@ export const informationFields = [
   {
     name: 'name',
     inputType: 'text',
-    disabled: true,
     label: 'Name',
   },
   {
     name: 'lastName',
     inputType: 'text',
-    disabled: true,
     label: 'LastName',
   },
   {
     name: 'age',
     inputType: 'number',
-    disabled: true,
     label: 'Age',
   },
   {
     name: 'address',
     inputType: 'text',
-    disabled: true,
     label: 'Address',
   },
   {
     name: 'cellphone',
     inputType: 'number',
-    disabled: true,
     label: 'CellPhone',
   },
 ];
@@ -35,19 +30,16 @@ export const subjectInformationFields = [
   {
     name: 'name',
     inputType: 'text',
-    disabled: true,
     label: 'Name',
   },
   {
     name: 'teacher',
     inputType: 'text',
-    disabled: true,
     label: 'Teacher',
   },
   {
     name: 'year',
     inputType: 'number',
-    disabled: true,
     label: 'Year',
   },
 ];
