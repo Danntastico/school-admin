eb423d9aa40e3a0c0225850049e9492e8726d25f
Danilo <nicolasdpf@gmail.com>
2020-08-16 16:57:07 -0500

    Implementing UseInput Hook

--- src/pages/StudentDetails.js
+++ src/pages/StudentDetails.js
@@ -1,41 +1,50 @@
-import React, { useState } from 'react';
-import { Headbar } from '../components/Headbar';
-import { CardContainer } from '../components/CardContainer';
-import { Form } from '../components/Form';
-import { informationFields } from '../utils/fieldsList';
-import { Field } from '../components/Field';
-import { Button } from '../components/Button';
-import { Table } from '../components/Table';
-import { TableItem } from '../components/TableItem';
-import { useInput } from '../hooks/useInput';
+import React, { useState } from "react";
+import { Headbar } from "../components/Headbar";
+import { CardContainer } from "../components/CardContainer";
+import { Form } from "../components/Form";
+import { informationFields } from "../utils/fieldsList";
+import { Field } from "../components/Field";
+import { Button } from "../components/Button";
+import { Table } from "../components/Table";
+import { TableItem } from "../components/TableItem";
+import { useInput } from "../hooks/useInput";
 
 export const StudentDetails = () => {
-  const [isDisabled, setIsDisabled] = useState(true)
-  const [ state, handleInputChange ] = useInput({
-    name: 'Danilo',
-    lastName: 'Peña',
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [state, handleInputChange] = useInput({
+    name: "Danilo",
+    lastName: "Peña",
     age: 24,
-    address: 'Cr 123 # 3- 321 sur',
-    cellphone: '34568985434'
-  })
+    address: "Cr 123 # 3- 321 sur",
+    cellphone: "34568985434",
+  });
 
   const handleClickEditFormBtn = (e) => {
     e.preventDefault();
     setIsDisabled(!isDisabled);
-  }
-
+  };
 
   return (
     <>
-      <Headbar title='Student Details' />
-      <div className='pageContent'>
+      <Headbar title="Student Details" />
+      <div className="pageContent">
         <CardContainer>
           <>
-            <Form title='Information' isEditable={true} onClick={handleClickEditFormBtn}>
+            <Form
+              title="Information"
+              isEditable={true}
+              onClick={handleClickEditFormBtn}
+            >
               {informationFields.map((i) => (
-                <Field key={i.name} value={state[i.name]} handleInputChange={handleInputChange} disabled={isDisabled} {...i} />
+                <Field
+                  key={i.name}
+                  value={state[i.name]}
+                  handleInputChange={handleInputChange}
+                  disabled={isDisabled}
+                  {...i}
+                />
               ))}
-              <Button label='Submit' isDisabled={isDisabled} />
+              <Button label="Submit" isDisabled={isDisabled} />
             </Form>
           </>
           <>
