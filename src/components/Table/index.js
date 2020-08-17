48153373aa809586a01be0889c909c12fa7965ba
Danilo <nicolasdpf@gmail.com>
2020-08-16 22:02:41 -0500

    Added new Props on Table COmponent

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { TableHeader, TableContent } from './styles';
-import { Button } from '../Button';
-import { CardContainer } from '../CardContainer';
+import React from "react";
+import { TableHeader, TableContent } from "./styles";
+import { Button } from "../Button";
 
-export const Table = ({ title = 'Items', children }) => {
+export const Table = ({
+  title = "Items",
+  children,
+  handleClick,
+  labelButton,
+}) => {
   return (
     <>
       <TableHeader>
         <h2>{title}</h2>
-        <Button>Add New Item</Button>
+        <Button onClick={handleClick} label={labelButton}>
+          Add New Item
+        </Button>
       </TableHeader>
       <TableContent>{children}</TableContent>
     </>
