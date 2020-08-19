7d52832c5f46e63b1b93c528424b5daa4fcb0799
Danilo <nicolasdpf@gmail.com>
2020-08-19 12:16:11 -0500

    Added item component!

--- src/containers/ListOfItems.js
+++ src/containers/ListOfItems.js
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { startFetchAllItems } from "../store/middlewares";
 import { List } from "../components/List";
+import { Item } from "../components/Item";
 
-export const ListOfItems = ({ openModal, PATH, isCourse }) => {
+export const ListOfItems = ({ openModal, PATH, DETAIL_PATH, isCourse }) => {
   const { data } = useSelector((state) => state[PATH]);
 
   const dispatch = useDispatch();
@@ -12,8 +13,16 @@ export const ListOfItems = ({ openModal, PATH, isCourse }) => {
   }, [PATH]);
 
   const fillData = isCourse
-    ? data.map((items) => <div key={items.id}> {items["name"]} </div>)
-    : data.map((items) => <div key={items.id}>{items["firstName"]}</div>);
+    ? data.map((items) => (
+        <Item to={`detail/${items.id}`} key={items.id}>
+          <p>{items["name"]}</p>
+        </Item>
+      ))
+    : data.map((items) => (
+        <Item to={`detail/${items.id}`} key={items.id}>
+          <p>{items["firstName"]}</p>
+        </Item>
+      ));
   return (
     <List
       title={`Active ${PATH}`}
