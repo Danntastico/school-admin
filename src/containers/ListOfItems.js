6c0beefd827c99cd229c28c8aa696d1462a24ab5
Danilo <nicolasdpf@gmail.com>
2020-08-19 11:49:36 -0500

    Updated containers

--- src/containers/ListOfItems.js
+++ src/containers/ListOfItems.js
@@ -3,25 +3,24 @@ import { useSelector, useDispatch } from "react-redux";
 import { startFetchAllItems } from "../store/middlewares";
 import { List } from "../components/List";
 
-export const ListOfItems = ({ openModal, PATH }) => {
-  const { data } = useSelector((state) => state.students);
+export const ListOfItems = ({ openModal, PATH, isCourse }) => {
+  const { data } = useSelector((state) => state[PATH]);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(startFetchAllItems(PATH));
   }, [PATH]);
-  console.log(PATH);
 
+  const fillData = isCourse
+    ? data.map((items) => <div key={items.id}> {items["name"]} </div>)
+    : data.map((items) => <div key={items.id}>{items["firstName"]}</div>);
   return (
-    <>
-      <List
-        title={`Active ${PATH}`}
-        buttonLabel="Add New Student"
-        handleAddItem={openModal}
-      >
-        {data.map((items) => (
-          <div key={items.id}> {items.firstName} </div>
-        ))}
-      </List>
-    </>
+    <List
+      title={`Active ${PATH}`}
+      buttonLabel={`Add New ${PATH}`}
+      handleAddItem={openModal}
+    >
+      {fillData}
+    </List>
   );
 };
--- src/store/middlewares/index.js
+++ src/store/middlewares/index.js
@@ -1,12 +1,20 @@
 import { getAllItems } from "../../utils/services/api";
-import { STUDENT_PATH } from "../../utils/constants";
+import { STUDENT_PATH, TEACHER_PATH, COURSE_PATH } from "../../utils/constants";
 import { getAllStudents } from "../actions/students";
+import { getAllTeachers } from "../actions/teachers";
+import { getAllCourses } from "../actions/courses";
 
 export const startFetchAllItems = (itemType) => async (dispatch) => {
   const items = await getAllItems(itemType);
   switch (itemType) {
     case STUDENT_PATH:
       dispatch(getAllStudents(items));
+      break;
+    case TEACHER_PATH:
+      dispatch(getAllTeachers(items));
+      break;
+    case COURSE_PATH:
+      dispatch(getAllCourses(items));
       break;
     default:
       break;
