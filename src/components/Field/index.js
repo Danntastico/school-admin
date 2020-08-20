c07b0d6064ce09197dd1f36d91d91c63d6830a7c
Danilo <nicolasdpf@gmail.com>
2020-08-20 08:43:59 -0500

    putting required params on Input fields

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -41,6 +41,7 @@ export const Field = ({
             placeholder={placeholder}
             type={inputType}
             value={value}
+            required={true}
           />
         );
     }
