10c186ba78809f0c750a5711088bfc6b5e594dbb
Danilo <nicolasdpf@gmail.com>
2020-08-21 09:34:59 -0500

    on Home window, dispatched all the data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { Headbar } from '../components/Headbar';
+import React, { useEffect } from "react";
+import { Headbar } from "../components/Headbar";
+import { useDispatch } from "react-redux";
+import { startGetAllItems } from "../store/middlewares";
+import {
+  STUDENTCOURSES_PATH,
+  COURSE_PATH,
+  TEACHER_PATH,
+  STUDENT_PATH,
+} from "../utils/constants";
 
 export const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(startGetAllItems(STUDENT_PATH));
+    dispatch(startGetAllItems(STUDENTCOURSES_PATH));
+    dispatch(startGetAllItems(COURSE_PATH));
+    dispatch(startGetAllItems(TEACHER_PATH));
+  }, []);
   return (
     <div>
-      <Headbar title='home' />
+      <Headbar title="home" />
     </div>
   );
 };
