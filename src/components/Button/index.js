9ec9a03026c6261099d5b82b8322c65afcfbdb3e
Danilo <nicolasdpf@gmail.com>
2020-08-21 16:18:36 -0500

    Organiced props for Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Btn } from "./styles";
 
-export const Button = ({ onClick, label = "Button", isDisabled }) => {
+export const Button = ({ type, onClick, label = "Button", isDisabled }) => {
   return (
-    <Btn onClick={onClick} disabled={isDisabled}>
+    <Btn onClick={onClick} type={type} disabled={isDisabled}>
       {label}
     </Btn>
   );
