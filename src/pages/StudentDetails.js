0365f47d1dadc4a72a398cd2889ee26a899ea53d
daniloWindows <nicolasdpf@gmail.com>
2020-08-24 18:12:20 -0500

    Structure for detail View

--- src/pages/StudentDetails.js
+++ src/pages/StudentDetails.js
@@ -1,22 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Headbar } from "../components/Headbar";
-import { useDispatch, useSelector } from "react-redux";
-import { STUDENT_PATH } from "../utils/constants";
-import { startGetItemById } from "../store/middlewares";
-import { useParams } from "@reach/router";
-import { useInput } from "../hooks/useInput";
 import { CardContainer } from "../styles/CardContainer";
-import { Button } from "../components/common/Button";
-import { InformationCard } from "../components/InformationCard";
-import { Form } from "../components/common/Form";
-import { Field } from "../components/common/Field";
-import { personInformationFields } from "../utils/fieldsList";
-
 export const StudentDetails = () => {
   return (
     <>
       <Headbar title="Student Details" />
-      <div className="pageContent"></div>
+      <div className="pageContent">
+        <CardContainer>
+          <div>
+            <div>
+              <div>
+                <h3>Information</h3>
+                <button>Edit</button>
+              </div>
+              <div>
+                <div>
+                  <p>Name</p>
+                  <p>My Name</p>
+                </div>
+                <div>
+                  <p>Name</p>
+                  <p>My Name</p>
+                </div>
+                <div>
+                  <p>Name</p>
+                  <p>My Name</p>
+                </div>
+                <div>
+                  <p>Name</p>
+                  <p>My Name</p>
+                </div>
+                <div>
+                  <p>Name</p>
+                  <p>My Name</p>
+                </div>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </CardContainer>
+      </div>
     </>
   );
 };
