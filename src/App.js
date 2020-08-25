a694dda18016582aa1d5337cc7e85b16dedb0e00
daniloWindows <nicolasdpf@gmail.com>
2020-08-24 20:53:12 -0500

    Wrapped root component with PersistGate provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { AppRouter } from "./routes/AppRouter";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store, persistor } from "./store";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <AppRouter />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppRouter />
+      </PersistGate>
     </Provider>
   );
 };
