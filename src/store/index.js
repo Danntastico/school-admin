9b8f710a966099cefdec2f2a2e3a272a985c74ff
daniloWindows <nicolasdpf@gmail.com>
2020-08-24 20:52:37 -0500

    Configured the Redux-persistor on store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
 import { dataReducer } from "./reducers/dataReducer";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
 const composeEnhancers =
   (typeof window !== "undefined" &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
@@ -10,7 +18,11 @@ const reducers = combineReducers({
   root: dataReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = createStore(
-  reducers,
+  persistedReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(store);
