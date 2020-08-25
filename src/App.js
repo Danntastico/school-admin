57ee455374ec1bddc27b6eefff9e4db4658df997
daniloWindows <nicolasdpf@gmail.com>
2020-08-25 01:32:42 -0500

    Removed persistor, cannot solve my problem

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { AppRouter } from "./routes/AppRouter";
-import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { store, persistor } from "./store";
+import { store } from "./store";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <AppRouter />
-      </PersistGate>
+      <AppRouter />
     </Provider>
   );
 };
