964c9beef64248cb56d80bd9ad958ca676dc0569
daniloWindows <nicolasdpf@gmail.com>
2020-08-25 01:24:07 -0500

    Implementing personInfoCard

--- src/pages/StudentDetails.js
+++ src/pages/StudentDetails.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Headbar } from "../components/Headbar";
 import { CardContainer } from "../styles/CardContainer";
-import { StudentInfoCard } from "../containers/InfoCards/StudentInfoCard";
+import { PersonInfoCard } from "../containers/InfoCards/PersonInfoCard";
+import { useParams } from "@reach/router";
+import { STUDENT_PATH } from "../utils/constants";
+import { startGetItemById } from "../store/middlewares";
 
 export const StudentDetails = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { students } = useSelector((state) => state.root);
+  const { activeStudent } = students;
+
+  const [studentDetails, setStudentDetails] = useState(activeStudent);
+
+  useEffect(() => {
+    dispatch(startGetItemById(STUDENT_PATH, id));
+  }, []);
+  useEffect(() => {
+    console.log("change!");
+    setStudentDetails(activeStudent);
+  }, [activeStudent]);
+
   return (
     <>
       <Headbar title="Student Details" />
       <div className="pageContent">
         <CardContainer>
-          <StudentInfoCard />
+          <PersonInfoCard
+            activePerson={studentDetails}
+            PATH={STUDENT_PATH}
+            id={id}
+            title="Student Information"
+          />
         </CardContainer>
       </div>
     </>
--- src/pages/TeacherDetails.js
+++ src/pages/TeacherDetails.js
@@ -1,60 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Headbar } from "../components/Headbar";
-import { Form } from "../components/common/Form";
-import { personInformationFields } from "../utils/fieldsList";
-import { Field } from "../components/common/Field";
-import { Button } from "../components/common/Button";
 import { CardContainer } from "../styles/CardContainer";
-import {
-  InformationCardItem,
-  InformationCard,
-} from "../components/InformationCard";
-import { useInput } from "../hooks/useInput";
+import { PersonInfoCard } from "../containers/InfoCards/PersonInfoCard";
+import { useParams } from "@reach/router";
+import { useSelector, useDispatch } from "react-redux";
+import { TEACHER_PATH } from "../utils/constants";
+import { startGetItemById } from "../store/middlewares";
 
 export const TeacherDetails = () => {
-  const [values, handleInputChange, reset] = useInput({});
-  const [isEditable, setIsEditable] = useState(false);
-
-  const handleOnEditClick = () => {
-    setIsEditable(!isEditable);
-  };
-  const renderForm = () => {
-    return (
-      <>
-        <Form hideHeader>
-          {personInformationFields.map((i) => (
-            <Field
-              key={i.name}
-              value={values[i.value]}
-              handleInputChange={handleInputChange}
-              {...i}
-            />
-          ))}
-          <Button />
-        </Form>
-      </>
-    );
-  };
-  const renderInformation = () => (
-    <>
-      <InformationCardItem label="Name" content="Danilo" />
-      <InformationCardItem label="Last Name" content="Danilo" />
-      <InformationCardItem label="Age" content="Danilo" />
-      <InformationCardItem label="Address" content="Danilo" />
-    </>
-  );
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { teachers } = useSelector((state) => state.root);
+  const { activeTeacher } = teachers;
+  console.log(activeTeacher);
+  useEffect(() => {
+    dispatch(startGetItemById(TEACHER_PATH, id));
+  }, [id, dispatch]);
 
   return (
     <div>
       <Headbar title="Teacher Details" />
       <div className="pageContent">
         <CardContainer>
-          <InformationCard
-            handleOnEditClick={handleOnEditClick}
+          <PersonInfoCard
+            activePerson={activeTeacher}
+            PATH={TEACHER_PATH}
+            id={id}
             title="Teacher Information"
-          >
-            {isEditable ? renderForm() : renderInformation()}
-          </InformationCard>
+          />
         </CardContainer>
       </div>
     </div>
