131069a1900d20a10fff908e2b86970021652b77
daniloWindows <nicolasdpf@gmail.com>
2020-08-25 14:09:14 -0500

    Added report button

--- src/components/Headbar/index.js
+++ src/components/Headbar/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Header } from './styles';
+import React from "react";
+import { Header } from "./styles";
+import { CsvReport } from "../../containers/CsvReport";
 
 export const Headbar = ({ title }) => {
   return (
     <Header>
       <h3>{title}</h3>
+      <CsvReport />
     </Header>
   );
 };
